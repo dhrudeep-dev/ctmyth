b633349af2bdc49b048ea91815f8ed128f6c9439
Dhruvil Modi <dhruvilmodi555@gmail.com>
2020-01-27 13:34:50 -0500

    tweenmax animation

--- js/app.js
+++ js/app.js
@@ -1,50 +1,78 @@
 $(document).foundation()
 $(document).ready(function() {
     $('#nav').localScroll(800);
-
     //.parallax(xPosition, speedFactor) options:
-
     //xPosition - Horizontal position of the element
-
     //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
-
-
-      //  featured page
+    
+    //  featured page
     $('#screen1_bgOne').parallax("50%", 0.1);
 
       //  myth 1
     $('#screen2_bgOne').parallax("50%", 0.1);
 
-    
       //  myth 2 
     $('#threeSceen').parallax("50%", 0.2);
   
-    
-
-
-
-
-
-
     //  myth 3 
     $('.screenFour').parallax("50%", 0.3);
 
-
-
-
-
-        //  myth 4
+    //  myth 4
     $('#screen5_bg').parallax("30%", 0.5);
 
-
-        //  myth 5
+    //  myth 5
     $('#screen6_bg').parallax("20%", 0.5);
-
-
-        //  myth 6
+  
+    //  myth 6
     $('#screen7_bg').parallax("20%", 0.3);
-
-
-
-
 })
+
+window.onload = function(){
+  // reference the pages
+  let body = document.querySelector("body");
+  // feature page
+  let introductionPage = document.querySelector('#screen1_bgOne');
+  let featureHeader = document.querySelector('.featuredHeader');
+  let downArrow = document.querySelector('#featurePageArrow');
+
+  // setting transofrm origins of all pages
+  TweenMax.set(featureHeader, {
+    transformOrigin: "center center"
+  });
+
+  TweenMax.set(downArrow, {
+    transformOrigin: "center center"
+  });
+
+  TweenMax.fromTo(introductionPage, 1, {
+    opacity: 0
+  },{
+    opacity:1,
+    delay: 1
+  });
+
+  TweenMax.fromTo(featureHeader, 1, {
+    opacity: 0,
+    scale: 0,
+  },{
+    opacity:1,
+    scale: 1,
+    delay: 2
+  });
+
+  TweenMax.fromTo(downArrow, 1, {
+    opacity: 0,
+  },{
+    opacity: 1,
+    delay: 3,
+  });
+
+  TweenMax.fromTo(downArrow, 1, {
+    y: 0,
+  },{
+    y: -5,
+    yoyo: true,
+    repeat: -1,
+    delay: 3,
+  });
+}
